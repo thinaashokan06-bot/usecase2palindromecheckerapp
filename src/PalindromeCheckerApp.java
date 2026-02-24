d2b0bdf0a155bd2a01b49f9dc130c14c87f91dc2
ktilak12 <tilakabhi0212.gmail.com>
2026-02-24 16:43:01 +0530

    UC6: Queue + Stack based palindrome checker demonstrating FIFO vs LIFO

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,3 +1,5 @@
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 import java.util.Stack;
 
@@ -6,21 +8,25 @@ public class PalindromeCheckerApp {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+
         Stack<Character> stack = new Stack<>();
+        Queue<Character> queue = new LinkedList<>();
 
         System.out.print("Enter a string to check palindrome: ");
         String input = scanner.nextLine();
 
-        // Push all characters into stack
+        // Insert characters into both stack and queue
         for (int i = 0; i < input.length(); i++) {
-            stack.push(input.charAt(i));
+            char ch = input.charAt(i);
+            stack.push(ch);       // LIFO
+            queue.add(ch);        // FIFO
         }
 
         boolean isPalindrome = true;
 
-        // Pop and compare
-        for (int i = 0; i < input.length(); i++) {
-            if (input.charAt(i) != stack.pop()) {
+        // Compare dequeue (queue) with pop (stack)
+        while (!stack.isEmpty()) {
+            if (!queue.remove().equals(stack.pop())) {
                 isPalindrome = false;
                 break;
             }
