8446d8886916bc657f1b1dc0d7f3c450a9aed2f8
thinaashokan06 <thinaashokan06@gmail.com>
2026-03-11 15:42:47 +0530

    UC8: palindrome checker

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -4,29 +4,28 @@ import java.util.Scanner;
 import java.util.Stack;
 
 public class PalindromeCheckerApp {
-
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
 
-        Stack<Character> stack = new Stack<>();
-        Queue<Character> queue = new LinkedList<>();
+        LinkedList<Character> list = new LinkedList<>();
 
         System.out.print("Enter a string to check palindrome: ");
         String input = scanner.nextLine();
 
-        // Insert characters into both stack and queue
+        // Add characters to linked list
         for (int i = 0; i < input.length(); i++) {
-            char ch = input.charAt(i);
-            stack.push(ch);       // LIFO
-            queue.add(ch);        // FIFO
+            list.add(input.charAt(i));
         }
 
         boolean isPalindrome = true;
 
-        // Compare dequeue (queue) with pop (stack)
-        while (!stack.isEmpty()) {
-            if (!queue.remove().equals(stack.pop())) {
+        while (list.size() > 1) {
+
+            char first = list.removeFirst();
+            char last = list.removeLast();
+
+            if (first != last) {
                 isPalindrome = false;
                 break;
             }
